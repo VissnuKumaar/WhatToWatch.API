8309ecab9d18feedb44cba59d6c0658b67647c57
Vissnu Kumaar Ravi <rvissnu101@gmail.com>
2020-03-03 15:48:49 -0500

    Implemented Lazy Loading
    Instead of sending random movies one by one. Constructed a list and sent a whole bunch of random movies.
    
    What is Lazy Loading?
    Lazy loading is a design pattern commonly used in computer programming to defer initialization of an object until the point at which it is needed. It can contribute to efficiency in the program's operation if properly and appropriately used. The opposite of lazy loading is eager loading.

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -19,9 +19,9 @@ namespace WhatToWatch.API.Controllers
         MovieHelper helper = new MovieHelper();
 
         [HttpPost]
-        public async Task<ActionResult<MovieResponse>> GetMovieDataAsync([FromBody]MovieRequest movieRequest)
+        public async Task<ActionResult<List<MovieResponse>>> GetMovieDataAsync([FromBody]MovieRequest movieRequest)
         {
-            MovieResponse apiResponse = new MovieResponse();
+            List<MovieResponse> randomMovies = new List<MovieResponse>();
             int year_diff = Convert.ToInt32(movieRequest.year_end) - Convert.ToInt32(movieRequest.year_start);
             int imdb_diff = Convert.ToInt32(movieRequest.imdb_end) - Convert.ToInt32(movieRequest.imdb_start);
             string stream_type = movieRequest.sources;
@@ -29,14 +29,17 @@ namespace WhatToWatch.API.Controllers
             int count = helper.randomCounter(stream_type, imdb_diff, year_diff);
             int result = 0;
             int counter = 0;
+            int tempSkip = 0;
             try
             {
                 do
                 {
                     Random random = new Random();
-                    int randSkip = random.Next(0, count);
-                    int randTake = random.Next(1, 50);
-                    var movieApiUrl = "https://******/" + movieRequest.sources + "?*********" + source + "&rt_end=100&rt_start=0&sort=4&sources=" + source;
+                    int randSkip = counter == 0 ? random.Next(0, count) : random.Next(0, tempSkip - 1);
+                    tempSkip = randSkip;
+                    int randTake = random.Next(1, 20);
+                    int randSort = random.Next(1, 3);
+                    var movieApiUrl = "****" + source + "***" + source + "****" + randSort + "***" + source;
                     movieApiUrl = movieApiUrl + "&genre=" + movieRequest.genre;
                     movieApiUrl = movieApiUrl + "&imdb_start=" + movieRequest.imdb_start;
                     movieApiUrl = movieApiUrl + "&imdb_end=" + movieRequest.imdb_end;
@@ -47,55 +50,65 @@ namespace WhatToWatch.API.Controllers
                     var client = new HttpClient();
                     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                     var response = await client.GetAsync(movieApiUrl);
-                    var model = await response.Content.ReadAsAsync<List<MovieResponse>>();
+                    List<MovieResponse> model = await response.Content.ReadAsAsync<List<MovieResponse>>();
                     if (counter > 15)
                     {
                         result = 1;
-                        apiResponse.status = false;
+                        randomMovies[0].status = false;
                         break;
                     }
                     if (model.Count != 0)
                     {
-                        result = 1;
-                        var movieDetailUrl = "https://api.reelgood.com/v3.0/content/movie/" + model[0].id + "?availability=onAnySource&interaction=true&region=us";
-                        var detailResponse = await client.GetAsync(movieDetailUrl);
-                        var detailModel = await detailResponse.Content.ReadAsStringAsync();
-                        var models = JsonConvert.DeserializeObject<test>(detailModel);
-                        var a = models.trailer;
-                        if (a == null)
+                        for (int modelCount = 0; modelCount < model.Count; ++modelCount)
                         {
-                            apiResponse.runtime = 0;
-                            apiResponse.trailer = null;
-                            apiResponse.applaunch = null;
-                        }
-                        else
-                        {
-                            var detailsModel = await detailResponse.Content.ReadAsAsync<MovieDetails>();
-                            apiResponse.runtime = detailsModel.runtime;
-                            apiResponse.trailer = detailsModel.trailer.key;
-                            for(int i = 0; i < 3; i++)
+                            MovieResponse apiResponse = new MovieResponse();
+                            result = 1;
+                            var movieDetailUrl = "******" + model[modelCount].id + "****";
+                            var detailResponse = await client.GetAsync(movieDetailUrl);
+                            var detailModel = await detailResponse.Content.ReadAsStringAsync();
+                            var models = JsonConvert.DeserializeObject<test>(detailModel);
+                            var a = models.trailer;
+                            if (a == null)
                             {
-                                if (detailsModel.availability[i].source_name == source)
+                                apiResponse.runtime = 0;
+                                apiResponse.trailer = null;
+                                apiResponse.applaunch = null;
+                            }
+                            else
+                            {
+                                var detailsModel = await detailResponse.Content.ReadAsAsync<MovieDetails>();
+                                apiResponse.runtime = detailsModel.runtime;
+                                apiResponse.trailer = detailsModel.trailer.key;
+                                for (int i = 0; i < 3; i++)
                                 {
-                                    apiResponse.applaunch = detailsModel.availability[i].source_data.links.web;
+                                    if (detailsModel.availability[i].source_name == source)
+                                    {
+                                        apiResponse.applaunch = detailsModel.availability[i].source_data.links.web;
+                                        apiResponse.android_launch = detailsModel.availability[i].source_data.links.android;
+                                        break;
+                                    }
                                 }
                             }
+                            apiResponse.status = true;
+                            apiResponse.id = model[modelCount].id;
+                            apiResponse.title = model[modelCount].title;
+                            apiResponse.overview = model[modelCount].overview;
+                            apiResponse.released_on = model[modelCount].released_on;
+                            apiResponse.imdb_rating = model[modelCount].imdb_rating;
+                            apiResponse.classification = model[modelCount].classification;
+                            apiResponse.poster_url = "****" + model[modelCount].id + "***";
+                            apiResponse.genre = movieRequest.genre_name;
+                            apiResponse.source = movieRequest.sources;
+                            randomMovies.Add(apiResponse);
                         }
-                        apiResponse.status = true;
-                        apiResponse.id = model[0].id;
-                        apiResponse.title = model[0].title;
-                        apiResponse.overview = model[0].overview;
-                        apiResponse.released_on = model[0].released_on;
-                        apiResponse.imdb_rating = model[0].imdb_rating;
-                        apiResponse.classification = model[0].classification;
-                        apiResponse.poster_url = "**********" + model[0].id + "/******.webp";
-                        apiResponse.genre = movieRequest.genre_name;
-                        apiResponse.source = movieRequest.sources;
                     }
                     else
                     {
                         ++counter;
-                        count = (counter > 10) ? (count - 10) : (counter > 13) ? 10 : count;
+                        if (counter >= 14) count = 1;
+                        else if (counter >= 12) count = 8;
+                        else if (counter >= 10) count = count - 100 < 0 ? Math.Abs(count - 100) : count - 100;
+                        else if (counter >= 5) count = count - 50 < 0 ? Math.Abs(count - 50) : count - 50;
                         continue;
                     }
 
@@ -103,9 +116,10 @@ namespace WhatToWatch.API.Controllers
             }
             catch (Exception e)
             {
-                apiResponse.status = false;
+                randomMovies[0].status = false;
             }
-            return apiResponse;
+            return randomMovies;
         }
     }
 }
+How to use ZIP Extractor

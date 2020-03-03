e3685fd372da6c1229592e6e6e7b82d307c2cb82
Vissnu Kumaar Ravi <rvissnu101@gmail.com>
2020-03-03 00:01:22 -0500

    Added Support to multiple streaming partners

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -6,6 +6,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using WhatToWatch.API.Helper;
 using WhatToWatch.API.Models;
 
 namespace WhatToWatch.API.Controllers
@@ -15,32 +16,27 @@ namespace WhatToWatch.API.Controllers
     public class MovieController : ControllerBase
     {
         static readonly HttpClient client = new HttpClient();
+        MovieHelper helper = new MovieHelper();
 
         [HttpPost]
         public async Task<ActionResult<MovieResponse>> GetMovieDataAsync([FromBody]MovieRequest movieRequest)
         {
             MovieResponse apiResponse = new MovieResponse();
-            var source = "netflix";
+            int year_diff = Convert.ToInt32(movieRequest.year_end) - Convert.ToInt32(movieRequest.year_start);
+            int imdb_diff = Convert.ToInt32(movieRequest.imdb_end) - Convert.ToInt32(movieRequest.imdb_start);
+            string stream_type = movieRequest.sources;
+            string source = (stream_type == "netflix") ? "netflix" : (stream_type == "amazon") ? "amazon_prime" : (stream_type == "disney") ? "disney_plus" : (stream_type == "hulu") ? "hulu_plus" : "hbo";
+            int count = helper.randomCounter(stream_type, imdb_diff, year_diff);
             int result = 0;
-            int count = 1000;
+            int counter = 0;
             try
             {
                 do
                 {
-                    if (movieRequest.sources == "hulu")
-                    {
-                        source = "hulu_plus";
-                        count = 300;
-                    }
-                    else if (movieRequest.sources == "amazon")
-                    {
-                        source = "amazon_prime";
-                        count = 300;
-                    }
                     Random random = new Random();
                     int randSkip = random.Next(0, count);
                     int randTake = random.Next(1, 50);
-                    var movieApiUrl = "*******" + movieRequest.sources + "*****"+source+"******" + source;
+                    var movieApiUrl = "https://******/" + movieRequest.sources + "?*********" + source + "&rt_end=100&rt_start=0&sort=4&sources=" + source;
                     movieApiUrl = movieApiUrl + "&genre=" + movieRequest.genre;
                     movieApiUrl = movieApiUrl + "&imdb_start=" + movieRequest.imdb_start;
                     movieApiUrl = movieApiUrl + "&imdb_end=" + movieRequest.imdb_end;
@@ -52,27 +48,62 @@ namespace WhatToWatch.API.Controllers
                     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                     var response = await client.GetAsync(movieApiUrl);
                     var model = await response.Content.ReadAsAsync<List<MovieResponse>>();
+                    if (counter > 15)
+                    {
+                        result = 1;
+                        apiResponse.status = false;
+                        break;
+                    }
                     if (model.Count != 0)
                     {
                         result = 1;
+                        var movieDetailUrl = "https://api.reelgood.com/v3.0/content/movie/" + model[0].id + "?availability=onAnySource&interaction=true&region=us";
+                        var detailResponse = await client.GetAsync(movieDetailUrl);
+                        var detailModel = await detailResponse.Content.ReadAsStringAsync();
+                        var models = JsonConvert.DeserializeObject<test>(detailModel);
+                        var a = models.trailer;
+                        if (a == null)
+                        {
+                            apiResponse.runtime = 0;
+                            apiResponse.trailer = null;
+                            apiResponse.applaunch = null;
+                        }
+                        else
+                        {
+                            var detailsModel = await detailResponse.Content.ReadAsAsync<MovieDetails>();
+                            apiResponse.runtime = detailsModel.runtime;
+                            apiResponse.trailer = detailsModel.trailer.key;
+                            for(int i = 0; i < 3; i++)
+                            {
+                                if (detailsModel.availability[i].source_name == source)
+                                {
+                                    apiResponse.applaunch = detailsModel.availability[i].source_data.links.web;
+                                }
+                            }
+                        }
+                        apiResponse.status = true;
+                        apiResponse.id = model[0].id;
+                        apiResponse.title = model[0].title;
+                        apiResponse.overview = model[0].overview;
+                        apiResponse.released_on = model[0].released_on;
+                        apiResponse.imdb_rating = model[0].imdb_rating;
+                        apiResponse.classification = model[0].classification;
+                        apiResponse.poster_url = "**********" + model[0].id + "/******.webp";
+                        apiResponse.genre = movieRequest.genre_name;
+                        apiResponse.source = movieRequest.sources;
                     }
                     else
                     {
+                        ++counter;
+                        count = (counter > 10) ? (count - 10) : (counter > 13) ? 10 : count;
                         continue;
                     }
-                    apiResponse.id = model[1].id;
-                    apiResponse.title = model[1].title;
-                    apiResponse.overview = model[1].overview;
-                    apiResponse.released_on = model[1].released_on;
-                    apiResponse.imdb_rating = model[1].imdb_rating;
-                    apiResponse.classification = model[1].classification;
-                    apiResponse.poster_url = "****" + model[1].id + "/poster-342.webp";
+
                 } while (result == 0);
             }
-            catch (HttpRequestException e)
+            catch (Exception e)
             {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
+                apiResponse.status = false;
             }
             return apiResponse;
         }

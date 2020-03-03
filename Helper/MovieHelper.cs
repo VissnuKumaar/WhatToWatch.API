830ad0605006fc7802acab8b64713e5312777910
Vissnu Kumaar Ravi <rvissnu101@gmail.com>
2020-03-03 15:51:03 -0500

    Improvised the randomization

--- Helper/MovieHelper.cs
+++ Helper/MovieHelper.cs
@@ -9,12 +9,10 @@ namespace WhatToWatch.API.Helper
     {
         public int randomCounter(string source, int imdb_diff, int year_diff)
         {
-            if (imdb_diff < 2 || year_diff < 2) return 30;
-            else if (imdb_diff < 3 || year_diff < 3) return 60;
-            else if (imdb_diff < 5 || year_diff < 5) return 100;
-            else if (source == "hbo" || source == "disney") return 500;
-            else if (source == "hulu" || source == "amazon") return 700;
-            else return 1000;
+            int imdbcount = imdb_diff < 2 ? 10 : imdb_diff < 3 ? 30 : imdb_diff < 4 ? 40 : imdb_diff < 5 ? 50 : 800;
+            int yearcount = year_diff < 2 ? 80 : year_diff < 3 ? 150 : year_diff < 4 ? 250 : year_diff < 5 ? 350 : 800;
+            int streamCount = source == "hbo" || source == "disney" ? 300 : source == "hulu" || source == "amazon" ? 500 : 800;
+            return Math.Min(Math.Min(imdbcount, yearcount), streamCount);
         }
     }
 }
